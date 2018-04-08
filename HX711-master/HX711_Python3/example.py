77b9d67cd08597bd3d81321d8ec29296ac6f6913
rikknijff <Rik.Knijff@rhea.infosupport.net>
2018-04-08 22:25:25 +0200

    added new libraries

--- HX711-master/HX711_Python3/example.py
+++ HX711-master/HX711_Python3/example.py
@@ -8,7 +8,7 @@ try:
 	# If you do not pass any argument 'gain_channel_A' then the default value is 128
 	# If you do not pass any argument 'set_channel' then the default value is 'A'
 	# you can set a gain for channel A even though you want to currently select channel B
-	hx = HX711(dout_pin=14, pd_sck_pin=15, gain_channel_A=128, select_channel='B')
+	hx = HX711(dout_pin=21, pd_sck_pin=20, gain_channel_A=128, select_channel='B')
 	
 	result = hx.reset()		# Before we start, reset the hx711 ( not necessary)
 	if result:			# you can check if the reset was successful
