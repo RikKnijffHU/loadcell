973a74e62755d0a13cc230264ea30b60b789eed7
Rik Knijff <Rik.Knijff@infosupport.com>
2018-04-09 16:26:42 +0200

    removed typecasting

--- SensorApp/Startup.py
+++ SensorApp/Startup.py
@@ -5,12 +5,12 @@ class Startup(object):
     """description of class"""
    
     def ReadEnviromentVariables(self):
-        productList: List[Product] = []
+        productList = []
         #enviromentList: List[str] = os.environ.get('Products').split(',')
-        enviromentList: List[str] = 'thee:2:14:15'.split(',')
+        enviromentList = 'thee:2:14:15'.split(',')
         for enviromentVariables in enviromentList:
             print(enviromentVariables) 
-            variable: List[str]  = enviromentVariables.split(':')
+            variable = enviromentVariables.split(':')
             product = Product(variable[0],variable[1],variable[2],variable[3])
             productList.append(product)
             print(len(enviromentList))    
