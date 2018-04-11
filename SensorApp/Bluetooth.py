fe8e160b3a771f1ac98d4c9760260077f34d9102
Rik Knijff <Rik.Knijff@infosupport.com>
2018-04-11 16:35:29 +0200

    send ble message to ble server

--- SensorApp/Bluetooth.py
+++ SensorApp/Bluetooth.py
@@ -2,18 +2,14 @@ import bluetooth
 class Bluetooth(object):
     """description of class"""
 
-    print("looking for nearby devices...")
-    nearby_devices = bluetooth.discover_devices(lookup_names = True, flush_cache = True, duration = 20)
-    print("found %d devices" % len(nearby_devices))
-    for addr, name in nearby_devices:
-        print(" %s - %s" % (addr, name))
-    for services in bluetooth.find_service(address = addr):
-        print(" Name: %s" % (services["name"]))
-        print(" Description: %s" % (services["description"]))
-        print(" Protocol: %s" % (services["protocol"]))
-        print(" Provider: %s" % (services["provider"]))
-        print(" Port: %s" % (services["port"]))
-        print(" Service id: %s" % (services["service-id"]))
-        print("")
-        print("")
+serverMACAddress = '28:3f:69:10:7c:94'
+port = 3
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.connect((serverMACAddress, port))
+while 1:
+    text = raw_input() # Note change to the old (Python 2) raw_input
+    if text == "quit":
+        break
+    s.send(text)
+sock.close()
 
