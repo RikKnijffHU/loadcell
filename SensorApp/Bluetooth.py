f4b0f1c8063db418038ab877ef5c5a19f9aadce8
Rik Knijff <Rik.Knijff@infosupport.com>
2018-04-11 17:03:50 +0200

    added ble server

--- SensorApp/Bluetooth.py
+++ SensorApp/Bluetooth.py
@@ -2,7 +2,7 @@ import bluetooth
 class Bluetooth(object):
     """description of class"""
 
-serverMACAddress = '28:3f:69:10:7c:94'
+serverMACAddress = 'B8:27:EB:E4:DA:19'
 port = 3
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.connect((serverMACAddress, port))
