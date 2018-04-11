62cf257fabb8009801c33c382ea0d569f9792289
Rik Knijff <Rik.Knijff@infosupport.com>
2018-04-11 16:40:09 +0200

    while loop added

--- SensorApp/Bluetooth.py
+++ SensorApp/Bluetooth.py
@@ -6,8 +6,8 @@ serverMACAddress = '28:3f:69:10:7c:94'
 port = 3
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.connect((serverMACAddress, port))
-while 1:
-    text = raw_input() # Note change to the old (Python 2) raw_input
+while True:
+    text = 'hoi' # Note change to the old (Python 2) raw_input
     if text == "quit":
         break
     s.send(text)
