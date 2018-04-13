93a13ead14095a09815a686e17cd6d4057b8f528
Rik Knijff <Rik.Knijff@infosupport.com>
2018-04-13 17:04:54 +0200

    ne uuid

--- Bluetooth/CentralModule.js
+++ Bluetooth/CentralModule.js
@@ -14,7 +14,7 @@ noble.startScanning(serviceUuids, allowDuplicates);
 noble.on('discover', function (peripheral) {
     peripheral.connect(function (error) {
         console.log('connected to peripheral: ' + peripheral.uuid);
-        peripheral.discoverServices([targetCharacteristic], function (error, services) {
+        peripheral.discoverServices(['8001'], function (error, services) {
             var immediateAlertService = services[0];
             console.log('discovered Immediate Alert service' + immediateAlertService.uuid);
 
