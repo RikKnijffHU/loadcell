53240530f6c16fea0d350a8ac71d73e55095a0c3
Rik Knijff <Rik.Knijff@infosupport.com>
2018-04-13 17:15:05 +0200

    added log

--- Bluetooth/CentralModule.js
+++ Bluetooth/CentralModule.js
@@ -20,12 +20,15 @@ noble.on('discover', function (peripheral) {
 
             immediateAlertService.discoverCharacteristics(['34cd'], function (error, characteristics) {
                 var alertLevelCharacteristic = characteristics[0];
+                for (var c in characteristics) {
+                    console.log(c.uuid)
+                }
                 console.log('discovered Alert Level characteristic');
 
                 // true if for write without response
-                alertLevelCharacteristic.write(new Buffer([0x01]), true, function (error) {
-                    console.log('set alert level to mid (1)');
-                });
+                //alertLevelCharacteristic.write(new Buffer([0x01]), true, function (error) {
+                 //   console.log('set alert level to mid (1)');
+               // });
             });
         });
     });
