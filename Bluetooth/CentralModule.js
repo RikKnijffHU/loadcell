9abd433c0f0d274659b63ea20fbbd212ce65c3b5
Rik Knijff <Rik.Knijff@infosupport.com>
2018-04-13 17:28:02 +0200

    update

--- Bluetooth/CentralModule.js
+++ Bluetooth/CentralModule.js
@@ -1,7 +1,7 @@
 ﻿var noble = require('noble');   //noble library
 var peripheralName = "Thee";     // the local name of the peripheral you want
 var targetService = '12ab';         // the service you want
-var targetCharacteristic = '000012AB-0000_1000_8000-00805F9B34FB';  // the characteristic you want
+var targetCharacteristic = '000012AB-0000-1000-8000-00805F9B34FB';  // the characteristic you want
 
 var serviceUuids = [targetService];
 
