4fbe936a36f497bd08f282954711d0d5bc4c6509
Rik Knijff <Rik.Knijff@infosupport.com>
2018-04-16 10:40:27 +0200

    added topicjs

---
+++ Bluetooth/topic.js
@@ -0,0 +1,15 @@
+var amqp = require('amqplib');
+var key =  'Inventory.RasberryService.VoorraadVeranderdEvent';
+var message = '{ArtikelNummer:1, NieuweVoorraad: 52}';
+
+amqp.connect('amqp://infosupport:1nfosupport@172.16.3.169').then(function(conn) {
+  return conn.createChannel().then(function(ch) {
+    var ex = 'InventoryBus';
+    var ok = ch.assertExchange(ex, 'topic', {durable: false});
+    return ok.then(function() {
+      ch.publish(ex, key, Buffer.from(message));
+      console.log(" [x] Sent %s:'%s'", key, message);
+      return ch.close();
+    });
+  }).finally(function() { conn.close(); })
+}).catch(console.log);
