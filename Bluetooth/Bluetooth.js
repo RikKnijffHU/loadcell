36e78e37d1f9c6d5b8696b6530eade17dc8b9054
rikknijff <Rik.Knijff@rhea.infosupport.net>
2018-04-18 00:19:09 +0200

    update

--- Bluetooth/Bluetooth.js
+++ Bluetooth/Bluetooth.js
@@ -51,7 +51,10 @@ class BluetoothPeripheralHandler {
                                     
                                     db.products.find(async function (err, docs) {
                                         // docs is an array of all the documents in mycollection
-                                        callback(bleno.Characteristic.RESULT_SUCCESS, new Buffer(docs.toArray()));
+                                        console.log(docs)
+                                        console.log(JSON.parse(docs))
+                                        console.log(JSON.stringify(docs))
+                                        callback(bleno.Characteristic.RESULT_SUCCESS, new Buffer(docs));
                                     });
                                     
                                 }
